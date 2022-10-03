534050146703b842efefebf8217e3dacf5368919
Ty_1991 <67247243+Ty1991twitter@users.noreply.github.com>
2022-10-03 01:20:00 -0400

    the actual final commit for the night

--- source/OptionsMenu.hx
+++ source/OptionsMenu.hx
@@ -8,7 +8,7 @@ class OptionsMenu extends MusicBeatState
 {
 	override function create()
 	{
-		var menuBG:FlxSprite = new FlxSprite().loadGraphic(AssetPaths.CD_Menu_png);
+		var menuBG:FlxSprite = new FlxSprite().loadGraphic(AssetPaths.CD_Menu__png);
 		menuBG.color = 0xFFea71fd;
 		menuBG.setGraphicSize(Std.int(menuBG.width * 1.1));
 		menuBG.updateHitbox();
